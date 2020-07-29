64b36662256f8da6a7be7d95db777cadeb7e4822
vaibhavAccionLabs <vaibhav.katariya@accionlabs.com>
2020-07-29 13:14:54 +0530

    Updates

---
+++ precache-manifest.92aa24167a436638d38f1a778ec67312.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caebbe20d1932b48db917a1c5f5fd2d7",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "8b0b1d2ab17a9fefddad",
+    "url": "/shopping-cart/static/css/2.8c467cd4.chunk.css"
+  },
+  {
+    "revision": "03b5c05971a2eccef6d5",
+    "url": "/shopping-cart/static/css/main.309c337c.chunk.css"
+  },
+  {
+    "revision": "8b0b1d2ab17a9fefddad",
+    "url": "/shopping-cart/static/js/2.3157e329.chunk.js"
+  },
+  {
+    "revision": "7166ec2d4c3501a61ab3",
+    "url": "/shopping-cart/static/js/3.acf74fee.chunk.js"
+  },
+  {
+    "revision": "59cf52b468f069f7985e",
+    "url": "/shopping-cart/static/js/4.02afa84e.chunk.js"
+  },
+  {
+    "revision": "747183867ac04f15e8c2",
+    "url": "/shopping-cart/static/js/5.196ce3ec.chunk.js"
+  },
+  {
+    "revision": "03b5c05971a2eccef6d5",
+    "url": "/shopping-cart/static/js/main.408fd46a.chunk.js"
+  },
+  {
+    "revision": "97a32f25c8e0e75918d9",
+    "url": "/shopping-cart/static/js/runtime~main.5e098c5a.js"
+  }
+]);
